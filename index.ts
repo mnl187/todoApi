4f1529b81a965e49b5c99d779c5b4c45618e7788
mnl187 <mnl187@gmail.com>
2022-10-24 16:58:14 +0200

    dodanie funkcji async dot. api

--- index.ts
+++ index.ts
@@ -4,3 +4,8 @@ interface SingleTodo {
     title: string;
     completed: boolean;
 }
+
+async function getTodo(id: number): Promise<SingleTodo> {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/todos/id');
+    return await resp.json();
+}
