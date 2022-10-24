05290b40ecedbe395669292ef44f125184839528
mnl187 <mnl187@gmail.com>
2022-10-24 17:03:43 +0200

    włączenie biblioteki DOM w Webstorm

--- index.ts
+++ index.ts
@@ -9,3 +9,8 @@ async function getTodo(id: number): Promise<SingleTodo> {
     const resp = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
     return await resp.json();
 }
+
+(async () => {
+    const todo = await getTodo(10);
+    console.log(todo.title, todo.completed ? 'is completed' : 'si to be done');
+})();
