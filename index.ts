b444af7b42f6af801a8b381c28c582d284d3c7f1
mnl187 <mnl187@gmail.com>
2022-10-24 17:25:31 +0200

    dodanie funkcji strzałkowej

--- index.ts
+++ index.ts
@@ -6,12 +6,16 @@ interface SingleTodo {
 }
 
 class TodoApi {
-    private readonly url = 'https://jsonplaceholder.typicode.com/todos/';
+    private readonly url = 'https://jsonplaceholder.typicode.com/todos';
 
     async get(id: number): Promise<SingleTodo> {
-        const resp = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        const resp = await fetch(`${this.url}/${id}`);
         return await resp.json();
     }
-
 }
+
+(async () => {
+    const todo = new TodoApi();
+    console.log(await todo.get(1));
+})()
 
